fd8b077e21c69532c561f2eddb0a988be0a977c0
22UCS097 <ravikumar628763@gmail.com>
2025-08-20 18:16:18 +0530

    addded contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 export default function Contact() {
+    
+
   return (
+    
     <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+      <p>CONTACT <span className='text-gray-700 font-semibold'>US</span> </p>
+      </div>
+
+       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+          <div className='flex-col justify-center items-startgap-6'>
+             <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+             <p  className='text-gray-500'>824101 jama Nagar <br /> Bihar, India 
+             </p>
+             <p className='text-gray-500'>Tel:(123) 456-0135 <br /> Email: abc@gmail.com </p>
+             <p className='font-semibold text-lg text'>CAREERS AT PRESCRIPTO</p>
+             <p>Learn more about our teams and jo openings. </p>
+             <button className='border border-black px-8 py-4 text-sm'>Explore Jobs</button>
+          </div>
+       </div>
       
     </div>
   )
