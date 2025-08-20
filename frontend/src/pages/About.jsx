cd106e1f8fbe83c938daf9df3b65f576c25a52e9
22UCS097 <ravikumar628763@gmail.com>
2025-08-20 17:54:04 +0530

    added about us section

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 export default function About() {
   return (
     <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium' >US</span></p>
+
+      </div>
+
+        <div className='my-10 flex flex-col md:flex-row gap-12'>
+          <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+           <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolorum natus dolorem accusamus dolor exercitationem ad expedita ipsum provident minima. Officiis quasi molestiae accusantium aut molestias ea et reiciendis reprehenderit?</p>
+             <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque odio deleniti enim obcaecati cupiditate temporibus amet excepturi rem quidem nam atque impedit velit at facilis accusantium exercitationem error, quia modi.</p>
+              <b className='text-gray-800'>Our Vision</b>
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum accusantium mollitia delectus atque veritatis numquam perferendis, excepturi aspernatur molestias qui, earum beatae facere sint. Dignissimos totam impedit quibusdam modi dolorem.</p>
+           </div>
+        </div>
+
+        <div className='text-xl my-4'>
+          <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span>  </p>
+        </div>
+
+        <div className='flex flex-col md:flex-row mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px]  hover:bg-blue-500 hover:text-white duration-300 text-gray-600 cursor-pointer '>
+             <b>Efficiency:</b>
+             <p>Steamlined appointment scheduling that fits into your busy lifestyle.</p>
+          </div>
+
+          <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-blue-500 hover:text-white duration-300 text-gray-600 cursor-pointer '>
+            <b>Convenience:</b>
+             <p>Access to a network of trusted healthcare professionals in your area.</p>
+          </div>
+
+          <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px]  hover:bg-blue-500 hover:text-white duration-300 text-gray-600 cursor-pointer '>
+            <b>Personalization:</b>
+             <p>Tailored recommendatins and reminder to help you stay on top of your health.</p>
+          </div>
+        </div>
       
     </div>
   )
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -28,16 +28,16 @@ export default function Doctors() {
   return (
     <div>
        <p className='text-greay-600'>Browse through the doctors specialist.</p>
-       <div className='flex flex-col sm:flex-row items-start gap-s mt-5'>
+       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
           <div className=' flex flex-col gap-4 text-sm textgray-600'>
                <p  onClick={()=>speciality==='General physician' ? navigate('/doctors'):navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 pr-16  py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="General physician" ? " bg-indigo-100 text-black" : ""}`}>General physician</p>
                <p onClick={()=>speciality==='Gynecologist' ? navigate('/doctors'):navigate('/doctors/Gynecologist')} className={`w-[94vw]sm:w-auto pl-3 pr-16 py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Gynecologist"?"bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
-               <p onClick={()=>speciality==='Dermatologist' ? navigate('/doctors'):navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3pr-16  py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Dermatologist"?"bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
-               <p onClick={()=>speciality==='Pediatricians' ? navigate('/doctors'):navigate('/doctors/Pediatricians')} className={`w-[94vw] sm:w-auto pl-3pr-16  py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Pediatricians"?"bg-indigo-100 text-black" : ""}`}>Pediatricians</p>
-               <p  onClick={()=>speciality==='Neurologist' ? navigate('/doctors'):navigate('/doctors/Neurologist')}className={`w-[94vw] sm:w-auto pl-3pr-16  py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Neurologist" ?"bg-indigo-100 text-black" : ""}`}>Neurologist</p>
+               <p onClick={()=>speciality==='Dermatologist' ? navigate('/doctors'):navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 *: pr-16  py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Dermatologist"?"bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
+               <p onClick={()=>speciality==='Pediatricians' ? navigate('/doctors'):navigate('/doctors/Pediatricians')} className={`w-[94vw] sm:w-auto pl-3 pr-16  py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Pediatricians"?"bg-indigo-100 text-black" : ""}`}>Pediatricians</p>
+               <p  onClick={()=>speciality==='Neurologist' ? navigate('/doctors'):navigate('/doctors/Neurologist')}className={`w-[94vw] sm:w-auto pl-3 pr-16  py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Neurologist" ?"bg-indigo-100 text-black" : ""}`}>Neurologist</p>
                <p onClick={()=>speciality==='Gastroenterologist' ? navigate('/doctors'):navigate('/doctors/Gastroenterologist')} className={`w-[94vw]sm:w-auto pl-3 pr-16 py-1.5 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="Gastroenterologist"?"bg-indigo-100 text-black" : ""}`}>Gastroenterologist</p>
           </div>
-          <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 gap-y-6'>
+          <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))]  gap-4 gap-y-4 '>
              {
               filterDoc.map((items,index)=>(
                       <div onClick={()=>navigate(`/appointment/${items._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] translation-all duration-500' key={index}>
