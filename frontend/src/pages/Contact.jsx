cd82f633845040cf2123be9027f7aec00ff34adc
22UCS097 <ravikumar628763@gmail.com>
2025-08-21 22:17:10 +0530

    added login and signUp pages

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,14 +13,14 @@ export default function Contact() {
 
        <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
          <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
-          <div className='flex-col justify-center items-startgap-6'>
+          <div className='flex flex-col justify-center items-start gap-6'>
              <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
              <p  className='text-gray-500'>824101 jama Nagar <br /> Bihar, India 
              </p>
              <p className='text-gray-500'>Tel:(123) 456-0135 <br /> Email: abc@gmail.com </p>
-             <p className='font-semibold text-lg text'>CAREERS AT PRESCRIPTO</p>
+             <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
              <p>Learn more about our teams and jo openings. </p>
-             <button className='border border-black px-8 py-4 text-sm'>Explore Jobs</button>
+             <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
           </div>
        </div>
       
